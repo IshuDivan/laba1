890498a86fed033e7dcabf474a0d05e4fd441746
IshuDivan <62930631+IshuDivan@users.noreply.github.com>
2020-07-01 12:08:12 +0300

    Update mylib.h
    now with classes and overloads!

--- mylib.h
+++ mylib.h
@@ -1,16 +1,20 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstdlib>
+
 using namespace std;
-struct bricks{
+class Bricks{
+public:
 	int  num;	
 	string form;
 	string colour;
+	friend std::ostream& operator<< (std::ostream &out, const Bricks &br);
+	friend std::istream& operator>> (std::istream &in,  Bricks &br);
 };
+std::ostream& operator<< (std::ostream &out, const Bricks &br);
 void add (string namefile);
 void search(string namefile);
 void deleting(string namefile);
 void allwrite(string namefile);
 void allclear(string namefile);
-
